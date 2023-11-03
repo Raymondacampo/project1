8f8a1bc5698d0ed671bdfcf79d931c3cff59a119
unknown <raymondacamposandoval@gmail.com>
2023-11-03 15:11:42 -0400

    ENTRY PAGE

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -3,8 +3,15 @@ from django.shortcuts import render
 from . import util
 
 
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, name):
+    name = name.upper()
+    if name in util.list_entries():
+        return render(request, "encyclopedia/entry.html", {
+            "entry": name, "content": util.get_entry(name)
+        })
