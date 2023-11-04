6d7e093489b91174e81cd837392b9d0d0454e9dc
unknown <raymondacamposandoval@gmail.com>
2023-11-04 12:28:28 -0400

    search feature

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:name>", views.entry, name="entry")
+    path("wiki/<str:name>", views.entry, name="entry"),
+    path('search/', views.search, name='search')
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from django.http import Http404
 from . import util
+from markdown2 import Markdown
 
-
+def md_to_html(title):
+    content = util.get_entry(title)
+    mark = Markdown()
+    if content == None:
+        return Null
+    else:
+        return mark.convert(content)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -12,5 +19,16 @@ def index(request):
 def entry(request, name):
     if name in util.list_entries():
         return render(request, "encyclopedia/entry.html", {
-            "entry": name, "content": util.get_entry(name)
+            "entry": name, "content": md_to_html(name)
         })
+
+def search(request):
+    if request.method == 'POST':
+        t = request.POST['q']
+        search_content = md_to_html(t)
+        if search_content is not None:
+            return render(request, "encyclopedia/entry.html", {
+                "entry": t, "content": search_content
+            })
+        else:
+            raise Http404
